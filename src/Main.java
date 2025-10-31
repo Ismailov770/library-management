c2b56f1bfd70bbd116fd0a3449d92200c734351e
Abdugafur <t7783076@gmail.com>
2025-10-31 18:23:48 +0500

    main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+
 public class Main {
     public static void main(String[] args) {
-        
+        LoggerService logger = new LoggerService();
+        SchedulerService scheduler = new SchedulerService(logger);
+
+        logger.log("INFO", "Logger service started");
+        scheduler.startScheduler();
+
+        System.out.println("✅ System started. Logs will appear in logs.txt");
     }
 }
